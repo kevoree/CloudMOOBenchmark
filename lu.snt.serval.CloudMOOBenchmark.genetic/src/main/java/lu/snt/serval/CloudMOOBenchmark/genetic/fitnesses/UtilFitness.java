48e0927e1cb96b24ea5b24437234ca9d1b845cd8
Assaad <assaad.moawad@uni.lu>
2014-05-27 15:24:24 +0200

    update

--- lu.snt.serval.CloudMOOBenchmark.genetic/src/main/java/lu/snt/serval/CloudMOOBenchmark/genetic/fitnesses/UtilFitness.java
+++ lu.snt.serval.CloudMOOBenchmark.genetic/src/main/java/lu/snt/serval/CloudMOOBenchmark/genetic/fitnesses/UtilFitness.java
@@ -1,11 +1,7 @@
-package lu.snt.serval.pla.genetic.fitnesses;
+package lu.snt.serval.CloudMOOBenchmark.genetic.fitnesses;
 
-import jet.runtime.typeinfo.JetValueParameter;
-import lu.snt.serval.pla.Blurring;
-import lu.snt.serval.pla.genetic.DomainConfiguration;
-import org.jetbrains.annotations.NotNull;
-import org.kevoree.ComponentInstance;
-import org.kevoree.ContainerRoot;
+
+import lu.snt.serval.cloud.Cloud;
 import org.kevoree.modeling.optimization.api.GenerationContext;
 import org.kevoree.modeling.optimization.api.fitness.FitnessFunction;
 import org.kevoree.modeling.optimization.api.fitness.FitnessOrientation;
@@ -19,43 +15,11 @@ import java.util.ArrayList;
  * University of Luxembourg - Snt
  * assaad.mouawad@gmail.com
  */
-public class UtilFitness extends DomainConfiguration implements FitnessFunction<ContainerRoot> {
+public class UtilFitness  extends FitnessFunction<Cloud> {
 
     @Override
-    public double evaluate(@JetValueParameter(name = "model") @NotNull ContainerRoot model, @JetValueParameter(name = "context") @NotNull GenerationContext<ContainerRoot> containerRootGenerationContext) {
-        ArrayList<ComponentInstance> ls = getSensors(model);
-        double localmax=1;
-        double cur=1;
-        for(ComponentInstance sensor: ls)
-        {
-            cur=1;
-            ArrayList<ComponentInstance> bl = getAttachedBlurComp(model,sensor);
-            for(ComponentInstance blur:bl)
-            {
-                String name = blur.getTypeDefinition().getName();
-                double value = Double.parseDouble(blur.getDictionary().findValuesByID("value").getValue());
-                cur=cur*getUtil(name, value);
-            }
-            if(cur<localmax)
-                localmax=cur;
-        }
-        return localmax;
-    }
-
-    private double getUtil(String name, double value) {
-        for(Blurring b: domain.getBlurrings())
-        {
-            if(b.getName().equals(name))
-            {
-                double aa= (b.getUtilMax()-b.getUtilMin())/(b.getParamMax()-b.getParamMin());
-                double bb= b.getUtilMax()-aa*b.getParamMax();
-                double util = aa*value+bb;
-                return util;
-
-            }
-        }
-        return 1;
-
+    public double evaluate(Cloud model, GenerationContext<Cloud> context) {
+        return 0;
     }
 
     @Override
@@ -68,8 +32,7 @@ public class UtilFitness extends DomainConfiguration implements FitnessFunction<
         return 0;
     }
 
-    @NotNull
-    @Override
+
     public FitnessOrientation orientation() {
         return FitnessOrientation.MAXIMIZE;
     }
--- lu.snt.serval.CloudMOOBenchmark.genetic/src/main/java/lu/snt/serval/CloudMOOBenchmark/genetic/mutators/DeleteBlurMutator.java
+++ lu.snt.serval.CloudMOOBenchmark.genetic/src/main/java/lu/snt/serval/CloudMOOBenchmark/genetic/mutators/DeleteBlurMutator.java
@@ -1,11 +1,8 @@
-package lu.snt.serval.pla.genetic.mutators;
+package lu.snt.serval.CloudMOOBenchmark.genetic.mutators;
 
 import jet.runtime.typeinfo.JetValueParameter;
-import lu.snt.serval.pla.genetic.DomainConfiguration;
+import lu.snt.serval.cloud.Cloud;
 import org.jetbrains.annotations.NotNull;
-import org.kevoree.*;
-import org.kevoree.kevscript.KevScriptEngine;
-import org.kevoree.log.Log;
 import org.kevoree.modeling.optimization.api.mutation.MutationOperator;
 import org.kevoree.modeling.optimization.api.mutation.MutationParameters;
 import org.kevoree.modeling.optimization.api.mutation.MutationVariable;
@@ -22,57 +19,23 @@ import java.util.List;
  * University of Luxembourg - Snt
  * assaad.mouawad@gmail.com
  */
-public class DeleteBlurMutator extends DomainConfiguration implements MutationOperator<ContainerRoot> {
+public class DeleteBlurMutator implements MutationOperator<Cloud> {
 
     @NotNull
     @Override
-    public List<MutationVariable> enumerateVariables(@JetValueParameter(name = "model") @NotNull ContainerRoot containerRoot) {
+    public List<MutationVariable> enumerateVariables(@JetValueParameter(name = "model") @NotNull Cloud containerRoot) {
         return Arrays.asList((MutationVariable) new QueryVar("target", "*"));
     }
 
     @NotNull
     @Override
-    public void mutate(@JetValueParameter(name = "model") @NotNull ContainerRoot model, @JetValueParameter(name = "params") @NotNull MutationParameters mutationParameters) {
+    public void mutate(@JetValueParameter(name = "model") @NotNull Cloud model, @JetValueParameter(name = "params") @NotNull MutationParameters mutationParameters) {
         try
         {
-            ArrayList<ComponentInstance> ls = getSensors(model);
-            ComponentInstance sensor= ls.get(random.nextInt(ls.size()));
-            ArrayList<ComponentInstance> already= getAttachedBlurComp(model, sensor);
-            if(already.size()>0)
-            {
-                ComponentInstance toRemove =already.get(random.nextInt(already.size()));
-                Channel before=toRemove.getProvided().get(0).getBindings().get(0).getHub();
-                Channel after=toRemove.getRequired().get(0).getBindings().get(0).getHub();
 
-                ComponentInstance next=toRemove;
-
-                for(MBinding c:after.getBindings())
-                {
-                    Port d = c.getPort();
-                    next= (ComponentInstance)  d.eContainer();
-                    if(next==toRemove)
-                        continue;
-                    else
-                        break;
-                }
-                KevScriptEngine engine = new KevScriptEngine();
-
-                String command = "unbind node0."+toRemove.getName()+"."+toRemove.getProvided().get(0).getPortTypeRef().getName() +" "+before.getName();
-                engine.execute(command,model);
-                command = "unbind node0."+toRemove.getName()+"."+toRemove.getRequired().get(0).getPortTypeRef().getName() +" "+after.getName();
-                engine.execute(command,model);
-                command = "remove node0." +toRemove.getName();
-                engine.execute(command,model);
-                command = "unbind node0."+next.getName()+"."+next.getProvided().get(0).getPortTypeRef().getName() +" "+after.getName();
-                engine.execute(command,model);
-                command = "bind node0."+next.getName()+"."+next.getProvided().get(0).getPortTypeRef().getName() +" "+before.getName();
-                engine.execute(command,model);
-
-            }
         }
         catch (Exception ex)
         {
-            Log.debug(ex.getMessage());
             System.out.println(ex.getMessage());
 
         }
